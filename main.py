4541d6da2489840b253f3880a6f679757d773ab8
Kristian Boda <kboda+git@protonmail.com>
2023-04-10 17:56:26 +0100

    lint: black formatting

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import time
@@ -8,31 +7,36 @@ import schedule
 from influxdb import InfluxDBClient
 
 # GLOBALS
-LIVE_CONN = bool(os.environ.get('LIVE_CONN',''))
-API_CLIENT = os.environ.get('API_CLIENT','')
-API_SECRET = os.environ.get('API_SECRET','')
-INFLUX_HOST = os.environ.get('INFLUX_HOST','')
-INFLUX_HOST_PORT = int(os.environ.get('INFLUX_HOST_PORT',''))
-INFLUX_DATABASE = os.environ.get('INFLUX_DATABASE','')
-LATITUDE = os.environ.get('LATITUDE','')
-LONGITUDE = os.environ.get('LONGITUDE','')
-RUNMINS =  int(os.environ.get('RUNMINS',1))
+LIVE_CONN = bool(os.environ.get("LIVE_CONN", ""))
+API_CLIENT = os.environ.get("API_CLIENT", "")
+API_SECRET = os.environ.get("API_SECRET", "")
+INFLUX_HOST = os.environ.get("INFLUX_HOST", "")
+INFLUX_HOST_PORT = int(os.environ.get("INFLUX_HOST_PORT", ""))
+INFLUX_DATABASE = os.environ.get("INFLUX_DATABASE", "")
+LATITUDE = os.environ.get("LATITUDE", "")
+LONGITUDE = os.environ.get("LONGITUDE", "")
+RUNMINS = int(os.environ.get("RUNMINS", 1))
 
-INFLUX_CLIENT = InfluxDBClient(host=INFLUX_HOST, port=INFLUX_HOST_PORT, database=INFLUX_DATABASE)
-JSON_OUTPUT = 'output.json'
+INFLUX_CLIENT = InfluxDBClient(
+    host=INFLUX_HOST, port=INFLUX_HOST_PORT, database=INFLUX_DATABASE
+)
+JSON_OUTPUT = "output.json"
+
 
 # Get saved json from MET
 def get_json(client_id, secret):
-    url = 'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/hourly?excludeParameterMetadata=false&includeLocationName=true&latitude={}&longitude={}'.format(LATITUDE, LONGITUDE)
+    url = "https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/hourly?excludeParameterMetadata=false&includeLocationName=true&latitude={}&longitude={}".format(
+        LATITUDE, LONGITUDE
+    )
     headers = {
-        'x-ibm-client-id': client_id,
-        'x-ibm-client-secret': secret,
-        'accept': 'application/json'
-        }
+        "x-ibm-client-id": client_id,
+        "x-ibm-client-secret": secret,
+        "accept": "application/json",
+    }
     resp = requests.get(url, headers=headers)
     payload_data = resp.json()
     print(payload_data)
-    with open(JSON_OUTPUT, 'w') as outfile:
+    with open(JSON_OUTPUT, "w") as outfile:
         json.dump(payload_data, outfile)
 
 
@@ -44,31 +48,33 @@ def get_saved_data(*args):
         working_data = json.load(json_file)
     return working_data
 
+
 def write_to_influx(data_payload):
     INFLUX_CLIENT.write_points(data_payload)
     pass
 
+
 def sort_json(working_data):
     # Interate over weather payload and pull out data points
-    data_points = working_data['features'][0]['properties']['timeSeries']
+    data_points = working_data["features"][0]["properties"]["timeSeries"]
     for data_point in data_points:
         # Cleans up for influxDB insert
-        base_dict = {'measurement' : 'met_weather', 'tags' : {'name': 'met_weather'}}
-        time_stamp = data_point['time']
-        base_dict.update({'time': time_stamp})
-        del data_point['time']
+        base_dict = {"measurement": "met_weather", "tags": {"name": "met_weather"}}
+        time_stamp = data_point["time"]
+        base_dict.update({"time": time_stamp})
+        del data_point["time"]
 
         # Make everything float to stop insert errors
-        for k,v in data_point.items():
+        for k, v in data_point.items():
             if type(v) == int:
-                data_point.update({k : float(v)})
+                data_point.update({k: float(v)})
 
-        base_dict.update({'fields' : data_point})
+        base_dict.update({"fields": data_point})
 
         # Construct payload and insert
         data_payload = [base_dict]
         print("SUBMIT:" + str(data_payload))
-        print('#'*30)
+        print("#" * 30)
         write_to_influx(data_payload)
 
 
@@ -76,8 +82,9 @@ def do_it():
     working_data = get_saved_data()
     sort_json(working_data)
 
+
 def main():
-    ''' Main entry point of the app '''
+    """Main entry point of the app"""
     do_it()
     schedule.every(RUNMINS).minutes.do(do_it)
 
@@ -86,6 +93,6 @@ def main():
         time.sleep(1)
 
 
-if __name__ == '__main__':
-    ''' This is executed when run from the command line '''
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
     main()
